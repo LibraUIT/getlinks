442503629c196e5e7cf117b80fef6c01c6579e36
Quan Nguyen <quanmn.libra@gmail.com>
2016-06-19 18:09:27 +0700

    Change to linknhanh.info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < ActionController::Base
   end
 
   def prepare_meta_tags(options={})
-    site_name   = "LINKS4U"
+    site_name   = "LINKNHANH.INFO"
     title       = "Get link JavHD, Generate link JavHD, Download JavHD, Generate Premium JavHD"
     description = "Get link JavHD và xem film online chất lượng HD | Generate link JavHD | Download JavHD | Generate Premium JavHD"
     image       = options[:image] || "https://lh3.googleusercontent.com/LvZNB0CzYHAkAYcBHbA2HHcUoBKC9JLToQOTKBC8dW70SAfO8K6Hrg2ZNdFjGvkJ37Dz9JlAy6vPxpk2KyEd9H5jqxUGh7LEaD8TA6V4AmpXmfi9QdJ_dQJNZNTohTXiUg5bYLoGjNn3TqvmBW3vup3HJbSAQCyQ7nm-tG-Z4k51647r4Z119VNtSK_tvvgt4NwqaMADMSRgd2824sn1qAmBGQ3Cn5TOpb1AHVDYE-dvTDfGeTYzCWa9MJrTqaqVhGYt8u4W8WUkDDk7y7KTRphJ3C5jMV-SBmG8oOC9xoUZEGxIlIjm-S6MC5SSxU9VI3D08I3eeVV_GjV2e8BtQgpWqDaeB8JFMRo0-nlKdgOaDoqBYi90JsUhOc07Qy4LHcRBk-I_wYSS7tzZyfkcvDnxr5mflPdzq4qgv81a-2iFSMw-vZ9H6o39ypLxWQpIWD7IcdN_Rl2aYu-9GfMRM6knW4adf7zJ9v0_nVCWp0oU1rAc9rB5RlwGaIYv5d-676nKH3QtRB8o6zPPY9sULoMBQlURuxeb8oucdCF7FH6lUl4egDFMJNp9RjW9vfQyD6nbr43JUKW74AJ05keTlFyWsIc6zjc=w271-h249-no"
@@ -53,7 +53,7 @@ class ApplicationController < ActionController::Base
       keywords:    %w[getlink Javhd.com javhd heymilf.com pussyav.com amateurav.com povav.com heyoutdoor.com lingerieav.com hairyav.com avstockings.com mediafire avanal.com avtits.com schoolgirlshd.com gangav.com ferame.com shiofuky.com av69.tv javhdonline download javhd tool],
       twitter: {
         site_name: site_name,
-        site: '@links4u',
+        site: '@linknhanh.info',
         card: 'summary',
         description: description,
         image: image
