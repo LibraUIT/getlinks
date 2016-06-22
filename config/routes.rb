090b01cd1dbb257a8773287817db9e61311114d8
Quan Nguyen <quanmn.libra@gmail.com>
2016-06-22 22:26:48 +0700

    Add Devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   post 'getlink', to: 'home#getlink'
   get :preview, to: 'home#preview'
   match "/404" => "errors#error404", via: [ :get, :post, :patch, :delete ]
+  devise_for :user, controllers: { sessions: 'user/sessions' }, path: '',
+                     module: :user,
+                     path_names: {
+                       sign_up: :signup,
+                       sign_in: :login,
+                       sign_out: :logout
+                     }
 end
